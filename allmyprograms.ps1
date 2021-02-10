417c1299ab47eae82b3028f70db21d046622c9aa
Rahul Arora <rarora7777@gmail.com>
2021-02-09 21:01:00 -0500

    Update allmyprograms.ps1

--- allmyprograms.ps1
+++ allmyprograms.ps1
@@ -3,75 +3,78 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 # firefox
-choco install firefox
+choco install firefox -y
 
 # vs2019
-choco install visualstudio2019community
-choco install visualstudio2019-workload-nativedesktop
+choco install visualstudio2019community -y
+choco install visualstudio2019-workload-nativedesktop -y
 
 # cmake
-choco install cmake
+choco install cmake -y
 
 # python3 (anaconda)
-choco install anaconda3
+choco install anaconda3 -y
 
 # unity hub
-choco install unity-hub
+choco install unity-hub -y
 
 # git
-choco install git
+choco install git -y
 refreshenv
 
+# Adobe CC
+choco install adobe-creative-cloud -y
+
 # blender
-choco install blender
+choco install blender -y
 
 # miktex
-choco install miktex
+choco install miktex -y
 
 # powershell core
-choco install powershell-core
+choco install powershell-core -y
 
 # rapid environment editor
-choco install rapidee
+choco install rapidee -y
 
 # dependency walker modern
-choco install dependencies
+choco install dependencies -y
 
 # notepad++
-choco install notepadplusplus
+choco install notepadplusplus -y
 
 # powertoys
-choco install powertoys
+choco install powertoys -y
 
 # 7zip
-choco install 7zip
+choco install 7zip -y
 
 # sumatra PDF
-choco install sumatrapdf
+choco install sumatrapdf -y
 
 # quicklook
-choco install quicklook
+choco install quicklook -y
 
 # conemu
-choco install conemu
+choco install conemu -y
 
 # ffmpeg
-choco install ffmpeg
+choco install ffmpeg -y
 
 # skype
-choco install skype
+choco install skype -y
 
 # zoom
-choco install zoom
+choco install zoom -y
 
 # franz
-choco install franz
+choco install franz -y
 
 # steam
-choco install steam
+choco install steam -y
 
-# google play music / YT music desktop player
-choco install gpmdp
+# google play music desktop player
+choco install gpmdp -y
 
 # vcpkg
 mkdir /dev
