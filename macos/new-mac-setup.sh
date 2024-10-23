43c181d75d79337a0cfcb871d5e5fb7cd6e6f449
Rahul Arora <rarora7777@gmail.com>
2024-10-22 19:47:47 -0500

    Minor

--- macos/new-mac-setup.sh
+++ macos/new-mac-setup.sh
@@ -1,3 +1,16 @@
+# Install homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# install git and git-related shell formatting stuff
 brew install git cmake
 brew install bash-completion zsh-autosuggestions
+brew install wget
+
+cd ~
+wget https://raw.githubusercontent.com/lyze/posh-git-sh/refs/heads/master/git-prompt.sh
+
+# install miniconda
+mkdir -p ~/miniconda3
+curl https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-arm64.sh -o ~/miniconda3/miniconda.sh
+bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
+rm ~/miniconda3/miniconda.sh
